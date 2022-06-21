7cdde44b4ca07b8a814580848bb0a131c7a7ad82
skandog <muumrar@gmail.com>
2022-06-21 11:31:39 +0100

    created dummy data in the libs folder

---
+++ libs/index.js
@@ -0,0 +1,32 @@
+const user = [
+  {
+    userId: 1,
+    name: "Claudia",
+    Bio: "I am a woman",
+    skillsGood: ["JavaScript", "node.js", "cypress"],
+    skillsBad: ["CSS", "React", "Backend"],
+  },
+];
+
+const skills = [
+  {
+    skillId: 1,
+    title: "JavaScript",
+    star: 4,
+    notes: "I be getting good at this",
+  },
+  {
+    skillId: 2,
+    title: "CSS",
+    star: 1,
+    notes: "I be getting bad at this",
+  },
+  {
+    skillId: 3,
+    title: "node.js",
+    star: 2,
+    notes: "I be getting ok at this",
+  },
+];
+
+export { user, skills };
