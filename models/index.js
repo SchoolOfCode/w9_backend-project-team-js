f3f0a3eef21370217f19b69f55bed863bc34cf05
skandog <muumrar@gmail.com>
2022-06-21 14:22:49 +0100

    made the router function async to await the heroku data

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
-import { query } from "../db/index.js"
+import { query } from "../db/index.js";
 
 export async function getUser() {
-    let result = await query(`SELECT * FROM userProfile`)
-    console.log(result);
-    return result;
+  let result = await query(`SELECT * FROM userProfile`);
+  console.log(result.rows);
+  return result.rows;
 }
-
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
-import  express  from "express";
-import { user } from "../libs/index.js"
+import express from "express";
+import { user } from "../libs/index.js";
 import { getUser } from "../models/index.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
-    const result = getUser();
-    const payload = { success: true, payload: result}
-    console.log("get came through");
-    res.json(payload);
-    
+userRouter.get("/", async (req, res) => {
+  const result = await getUser();
+  console.log(result);
+  const payload = { success: true, payload: result };
+  console.log("get came through");
+  res.json(payload);
 });
 
 export default userRouter;
