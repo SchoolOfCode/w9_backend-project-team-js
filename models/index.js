6933d07041e3277172ea0c0985c79d946c92d55f
skandog <muumrar@gmail.com>
2022-06-22 16:08:30 +0100

    created post route and logic for new goals

--- models/index.js
+++ models/index.js
@@ -37,3 +37,23 @@ export async function createSkill(newSkill) {
 
   return responseObject;
 }
+
+// create new goal
+export async function createGoal(newGoal) {
+  const sqlString = `INSERT INTO goals (details, complete, notes) VALUES($1, $2, $3) RETURNING *;`;
+
+  for (let i = 0; i < newGoal.length; i++) {
+    const params = [newGoal[i].details, newGoal[i].complete, newGoal[i].notes];
+    const res = await query(sqlString, params);
+
+    console.log(res.rows[0].title, "Has been inserted");
+  }
+
+  // moved responseObject here since API response was wrongly showing previous array (before push)
+  const responseObject = {
+    success: true,
+    payload: `new goals was added to database`,
+  };
+
+  return responseObject;
+}
--- routes/goals.js
+++ routes/goals.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getGoals } from "../models/index.js";
+import { getGoals, createGoal } from "../models/index.js";
 
 const goalsRouter = express.Router();
 
 goalsRouter.get("/", async (req, res) => {
   const result = await getGoals();
   const payload = { success: true, payload: result };
+  res.json(payload);
+});
+
+goalsRouter.post("/", async (req, res) => {
+  const body = req.body;
+  const payload = await createGoal(body);
   res.json(payload);
 });
 
