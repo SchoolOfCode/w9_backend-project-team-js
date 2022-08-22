d4ba928e5db9569473f94ea514d747514c80dbc6
Adam P <adam.phasey@hotmail.com>
2022-08-22 14:04:12 +0100

    refactored pg host to have only database url to try to get this deployed

--- db/index.js
+++ db/index.js
@@ -1,14 +1,21 @@
 import pg from "pg";
 import "dotenv/config";
 
-export const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+// export const pool = new pg.Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
 
-  ssl: { rejectUnauthorized: false },
+//   ssl: { rejectUnauthorized: false },
+// });
+
+export const pool = new pg.Pool({
+  connectionString: db.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export function query(text, params, callback) {
